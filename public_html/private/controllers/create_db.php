7e7815ca6fbabd06435864bf694f90140bcb0798
Chris Baur <chriskbaur@gmail.com>
2018-01-28 00:22:10 -0500

    made accounts table
    Made accounts table SQL, added code to create_db file

--- public_html/private/controllers/create_db.php
+++ public_html/private/controllers/create_db.php
@@ -17,8 +17,27 @@ try {
     $log->lwrite('Connected successfully');
 	
 	//create accounts
-	
-    }
+	$sql = "DROP TABLE IF EXISTS `accounts`;CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL DEFAULT 'john117',
+  `password` varchar(500) DEFAULT NULL,
+  `name` varchar(25) NOT NULL DEFAULT 'default name',
+  `last_name` varchar(25) NOT NULL DEFAULT 'default  last',
+  `gender` set('male','female','other') NOT NULL DEFAULT 'other',
+  `security_one` set('q1','q2','q3') DEFAULT 'q1',
+  `security_two` set('q1','q2','q3') NOT NULL DEFAULT 'q1',
+  `answer_one` varchar(20) NOT NULL DEFAULT 'a1',
+  `answer_two` varchar(20) NOT NULL DEFAULT 'a1',
+  `bio` varchar(500) NOT NULL DEFAULT 'Training to be like goku',
+  `profession` set('gamer','student','potatoe','teacher','professor','hipster','trainer','sloth') NOT NULL DEFAULT 'potatoe',
+  `pin` tinyint(4) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1";
+
+	$conn->exec($sql);
+	$log->lwrite('Table accounts created successfully');
+}
 catch(PDOException $e)
     {
     $log->lwrite('Connection failed: ' . $e->getMessage());
