b3bf09b9f3a8c23cd2fc82da1b8d553bd2b41a87
chris-baur <chriskbaur@gmail.com>
2018-02-02 16:45:55 -0500

    edited paths to reflect file structure change, made Questons table
    Some paths in the php files were still using the old structure. Now they are up to date and should work. Also, I made the Questions table and added the SQL to the create_db file

---
+++ accounts.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 02, 2018 at 09:38 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ua1_so`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL DEFAULT 'john117',
+  `password` varchar(500) DEFAULT NULL,
+  `name` varchar(25) NOT NULL DEFAULT 'default name',
+  `last_name` varchar(25) NOT NULL DEFAULT 'default  last',
+  `gender` set('Male','Female','Other') NOT NULL DEFAULT 'Male',
+  `security_one` set('What is the first name of the person you first kissed?','What is the last name of the teacher who gave you your first failing grade?','What is your pets name?','What was the name of your elementary / primary school?','In what city or town does your nearest sibling live?','What was your childhood nickname?','What is the name of your favorite childhood friend?') DEFAULT 'What is the first name of the person you first kissed?',
+  `security_two` set('In what city or town did your mother and father meet?','What is the middle name of your oldest child?','What is your favorite team?','What is your favorite movie?','What was your favorite sport in high school?','What was your favorite food as a child?','Who is your childhood sports hero?','What was the name of the company where you had your first job?') NOT NULL DEFAULT 'In what city or town did your mother and father meet?',
+  `answer_one` varchar(20) NOT NULL DEFAULT 'a1',
+  `answer_two` varchar(20) NOT NULL DEFAULT 'a1',
+  `bio` varchar(500) NOT NULL DEFAULT 'Training to be like goku',
+  `profession` set('Gamer','Student','Potatoe','Teacher','Professor','Hipster','Trainer','Sloth') NOT NULL DEFAULT 'Potatoe',
+  `pin` tinyint(4) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- private/controllers/create_db.php
+++ private/controllers/create_db.php
@@ -1,8 +1,12 @@
 <?php
 
+/**
+ * @author Christoffer Baur
+ */
+
 include '..\util\logging.php';
 include '..\util\sets.php';
-$config = parse_ini_file('..\..\..\config.ini');
+$config = parse_ini_file('..\..\config.ini');
 
 $servername = $config['servername'];
 $username = $config['username'];
@@ -22,6 +26,7 @@ $professions = $sets->to_string_professions();
 function createDatabaseAndTales(){
     createDatabase();
     createAccountsTable();
+    createQuestionsTable();
 }
 
 function createDatabase(){
@@ -45,27 +50,27 @@ function createAccountsTable(){
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
-        //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $log->lwrite('Connected successfully');
 
         //create accounts
         $sql = "DROP TABLE IF EXISTS `accounts`;CREATE TABLE IF NOT EXISTS `accounts` (
-    `id` int(11) NOT NULL,
-    `username` varchar(20) NOT NULL DEFAULT 'john117',
-    `password` varchar(500) DEFAULT NULL,
-    `name` varchar(25) NOT NULL DEFAULT 'default name',
-    `last_name` varchar(25) NOT NULL DEFAULT 'default  last',
-    `gender` set($genders) NOT NULL DEFAULT '".$sets->get_genders()[0]."',
-    `security_one` set($security_one) DEFAULT '".$sets->get_security_one()[0]."',
-    `security_two` set($security_two) NOT NULL DEFAULT '".$sets->get_security_two()[0]."',
-    `answer_one` varchar(20) NOT NULL DEFAULT 'a1',
-    `answer_two` varchar(20) NOT NULL DEFAULT 'a1',
-    `bio` varchar(500) NOT NULL DEFAULT 'Training to be like goku',
-    `profession` set($professions) NOT NULL DEFAULT '".$sets->get_professions()[2]."',
-    `pin` tinyint(4) UNSIGNED NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `username` (`username`)
-    ) ENGINE=MyISAM DEFAULT CHARSET=latin1";
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `username` varchar(20) NOT NULL DEFAULT 'john117',
+            `password` varchar(500) DEFAULT NULL,
+            `name` varchar(25) NOT NULL DEFAULT 'default name',
+            `last_name` varchar(25) NOT NULL DEFAULT 'default  last',
+            `gender` set($genders) NOT NULL DEFAULT '".$sets->get_genders()[0]."',
+            `security_one` set($security_one) DEFAULT '".$sets->get_security_one()[0]."',
+            `security_two` set($security_two) NOT NULL DEFAULT '".$sets->get_security_two()[0]."',
+            `answer_one` varchar(20) NOT NULL DEFAULT 'a1',
+            `answer_two` varchar(20) NOT NULL DEFAULT 'a1',
+            `bio` varchar(500) NOT NULL DEFAULT 'Training to be like goku',
+            `profession` set($professions) NOT NULL DEFAULT '".$sets->get_professions()[2]."',
+            `pin` tinyint(4) UNSIGNED NULL DEFAULT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `username` (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
         $conn->exec($sql);
         $log->lwrite('Table accounts created successfully');
@@ -74,6 +79,38 @@ function createAccountsTable(){
     catch(PDOException $e)
     {
     $log->lwrite('Connection failed: ' . $e->getMessage());
+    }finally{
+        unset ($pdo);
+    }
+}
+
+function createQuestionsTable(){
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $log->lwrite('Connected successfully');
+
+        //create Questions Table
+        $sql = "DROP TABLE IF EXISTS `questions`;
+            CREATE TABLE IF NOT EXISTS `questions` (
+            `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+            `header` varchar(100) NOT NULL DEFAULT 'Ain''t it Fun',
+            `content` varchar(500) NOT NULL DEFAULT 'Brick by Boring Brick',
+            `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `upvotes` mediumint(8) UNSIGNED NOT NULL DEFAULT '0',
+            `downvotes` mediumint(8) UNSIGNED NOT NULL DEFAULT '0',
+            `tags` varchar(100) NOT NULL DEFAULT 'noob',
+            PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+        $conn->exec($sql);
+        $log->lwrite('Questions Table created successfully');
+        
+    }
+    catch(PDOException $e)
+    {
+        $log->lwrite('Connection failed: ' . $e->getMessage());
     }finally{
         unset ($pdo);
     }
