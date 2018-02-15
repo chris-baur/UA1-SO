e6b953f8e83906aba29e343f96630e8c0a16a67b
c-concio <c-concio@users.noreply.github.com>
2018-02-14 19:08:42 -0500

    Fixed up the footer format
    Changed up the format of the footer elements and made the logos line up horizontally insted of vertically

--- public_html/home_page/footer.php
+++ public_html/home_page/footer.php
@@ -1,30 +1,52 @@
 <header id="footer">
       <div class="container">
         <a href="footer.html"></a>
-        <table align="center" style="height: 184px;">
-        <tr>
-          <!--<th><img src="../img/logo.png" alt="" width="240" height="130" /></th>-->
-          <th><br>Conctact us&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
-          <th><br>For any inquiries, please email us&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </th> 
-          <th><br>Follow us</th>
-        </tr>
-        <tr>
-          <td>Email us: <a href="mailto:contact@asktit.com">contact@asktit.com</a></td>
-          <td><a href="mailto:help@askit.com">help@askit.com</a>
-          <td><img src="../img/facebook.png" alt="" width="20" height="20" /></td>
-        </tr>
-        <tr>
-          <td>Phone CA: (514) xxx-xxxx&nbsp;</td>
-          <td> </td>
-          <td><img src="../img/instagram.png" alt="" width="20" height="20" /></td>
-        </tr>
-        <tr>
-          <td>   </td>
-          <td>   </td>
-          <td><img src="../img/twitter.png" alt="" width="20" height="20" /></td>
-        </tr>
-        </table>
+            <table class = "footerTable" align = "center">
+              <col width = "250px">
+              <col width = "340px">
+
+              <!--Table titles-->
+              <thead>
+                <th style = "height:40px">CONTACT US</th>
+                <th>FOR ANY INQUIRIES PLEASE EMAIL</th>
+                <th>FOLLOW US</th>
+              </thead>
+ 
+              <tbody>
+                <!--Contact us content-->
+                <td>
+                  <table>
+                    <tr>
+                      <td style = "height: 20px; vertical-align: top">Email us: <a href="mailto:contact@asktit.com">contact@askit.com</a></td>
+                    </tr>
+                    <tr>
+                      <td style = "vertical-align: top; height: 50px">Phone CA: (514) xxx-xxxx&nbsp;</td>
+                    </tr>
+                  </table>
+                </td>
+
+                <!--Inquiries content-->
+                <td style = "vertical-align: top"><a href="mailto:help@askit.com">help@askit.com</a></td>
+
+                <!--Follow us content-->
+                <td style = "vertical-align: top">
+                  <table>
+                    <td><img src="../img/facebook.png" alt="" width="20" height="20" /></td>
+                    <td><img src="../img/instagram.png" alt="" width="20" height="20" /></td>
+                    <td><img src="../img/twitter.png" alt="" width="20" height="20" /></td>
+                  </table>
+                </td>
+              </tbody>
+            </table>
       </div>
     </header>
   </body>
+
+  <!-- changing style in homepage.css not working for the moment so going to style table here -->
+  <style>
+    .footerTable {
+        font-family: "Arial";
+    }
+  </style>
+
 </html>
