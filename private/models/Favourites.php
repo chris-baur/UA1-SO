6f24f30d123b8bd8900a1912c14671fef802e6f7
chris-baur <chriskbaur@gmail.com>
2018-02-15 08:21:10 -0500

    Made favourites model
    The model representing the favourites table hasd been made.

---
+++ private/models/Favourites.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @author Christoffer Baur
+ * 
+ * Model representation of the favourties table
+ * 
+ */
+
+ class Favourites{
+    //class variables
+    var $id;
+    var $accountId;
+    var $questionId;
+    var $answerId;
+
+    function __contruct($id = 0, $accoundId = 0, $questionId = 0, $answerId = 0){
+        $this->id = $id;
+        $this->accountId = $accountId;
+        $this->questionId = $questionId;
+        $this->answerId = $answerId;
+    }
+
+    function get_id(){
+        return $this->id;
+    }
+
+    function get_accountId(){
+        return $this->accountId;
+    }
+
+    function get_questionId(){
+        return $this->questionId;
+    }
+
+    function get_answerId(){
+        return $this->answerId;
+    }
+
+    function set_id($newId){
+        $this->id = $newId;
+    }
+
+    function set_accountId($newAccountId){
+        $this->accountId = $newAccountId;
+    }
+
+    function set_questionId($newQuestionId){
+        $this->questionId = $newQuestionId;
+    }
+
+    function set_answerId($newAnswerId){
+        $this->answerId = $newAnswerId;
+    }
+
+
+ }
+
+
+?>
