d4afc91cf6de72bdbf0afd3e4bdc2502957b77c2
chris-baur <chriskbaur@gmail.com>
2018-02-17 17:36:23 -0500

    edited test file include statement
    Trying to make travis-ci build succeed

--- tests/questionsTest.php
+++ tests/questionsTest.php
@@ -1,7 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-require '..\private\models\Question.php';
+include '..\private\models\Question.php';
 final class questionModelTest extends TestCase{
     public function testOnePlusOne() {
         $this->assertEquals(1+1,2);
