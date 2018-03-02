cbf506515fb8d98b8c2cc69de5127102e77d6a46
chris-baur <chriskbaur@gmail.com>
2018-03-02 12:04:19 -0500

    Changed some files to test travis-CI build
    travis ml file changed and test class modified to reflect changes to phpunit namespace change since php 6.0.

--- tests/questionsTest.php
+++ tests/questionsTest.php
@@ -1,8 +1,7 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
 //include '..\private\models\Question.php';
-class questionModelTest extends PHPUnit_Framework_TestCase{
+class questionModelTest extends PHPUnit\Framework\TestCase{
     public function testOnePlusOne() {
         $int = 1+1;
         $expected = 2;
