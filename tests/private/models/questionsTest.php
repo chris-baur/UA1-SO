d0e6568215d9d1e6347675b20ed0c6945a58ca41
chris-baur <chriskbaur@gmail.com>
2018-03-03 21:13:39 -0500

    changed path
    changed path
    
    Issue #11

--- tests/private/models/questionsTest.php
+++ tests/private/models/questionsTest.php
@@ -2,7 +2,7 @@
 
 //require_once '..\private\models\Question.php';
 //require_once '..\private\controllers\create_db.php';
-require __DIR__ . "../../../private/models/Question.php";
+require __DIR__ . "UA1-SO/private/models/Question.php";
 
 class questionModelTest extends PHPUnit\Framework\TestCase{
     public function testOnePlusOne() {
