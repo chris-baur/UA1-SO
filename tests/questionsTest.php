b6d4b5a3e64695080613d09a44112545bb15da51
chris-baur <chriskbaur@gmail.com>
2018-03-03 20:36:22 -0500

    Edited test file, and travis file
    testing question class.
    Issue #11

--- tests/questionsTest.php
+++ tests/questionsTest.php
@@ -1,14 +1,16 @@
 <?php
 
-//include '..\private\models\Question.php';
+require_once '..\private\models\Question.php';
+require_once '..\private\controllers\create_db.php';
+
 class questionModelTest extends PHPUnit\Framework\TestCase{
     public function testOnePlusOne() {
         $int = 1+1;
         $expected = 2;
         $this->assertEquals($int,$expected);
     }
-    // public function testConstructor(){
-    //     $q = new Question();
-    //     $this->assertNotNull($q);
-    // }
+    public function testConstructor(){
+        $q = new Question();
+        $this->assertNotNull($q);
+    }
 }
