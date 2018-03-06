cee111be62010c18212ea5f0af455d9e6d5ac755
chris-baur <chriskbaur@gmail.com>
2018-03-06 08:14:41 -0500

    Edited travis ile
    tried changing whitelist directory

--- tests/private/models/questionsTest.php
+++ tests/private/models/questionsTest.php
@@ -1,9 +1,5 @@
 <?php
 
-//require_once '..\private\models\Question.php';
-//require_once '..\private\controllers\create_db.php';
-require __DIR__ . "UA1-SO/private/models/Question.php";
-
 class questionModelTest extends PHPUnit\Framework\TestCase{
     public function testOnePlusOne() {
         $int = 1+1;
