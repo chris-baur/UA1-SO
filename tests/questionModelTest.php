3d095346e5e368d84b809928a2a508050e41a51c
chris-baur <chriskbaur@gmail.com>
2018-03-06 16:50:49 -0500

    Removed basic function added answer model test
    removed basic function that was used for early testing. Added the answerModelTest file.

--- tests/questionModelTest.php
+++ tests/questionModelTest.php
@@ -1,11 +1,6 @@
 <?php
 
 class questionModelTest extends PHPUnit\Framework\TestCase{
-    public function testOnePlusOne() {
-        $int = 1+1;
-        $expected = 2;
-        $this->assertEquals($int,$expected);
-    }
     public function testConstructor(){
         $q = new Question();
         $this->assertNotNull($q);
