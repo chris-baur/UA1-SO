70de938fb367e8535aaa78ac8c1add79312dbfdd
c-concio <c-concio@users.noreply.github.com>
2018-03-16 02:22:37 -0400

    Question Thread Page added when clicking on a question header
    Only added the question block of the thread page has currently been implemented

--- private/controllers/question_controller.php
+++ private/controllers/question_controller.php
@@ -189,6 +189,54 @@ $log = new Logging();
 	}
 
 	/**
+	 * Returns questions with the specified id
+	 *
+	 * @param $header		Question's header
+	 */
+	function getQuestionsById($id){
+		global $servername, $username, $password, $dbname, $log;
+        $question = new Question();
+        $questionArray = [];
+		
+		try{
+			$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+			$stmt = $pdo -> prepare("SELECT id, account_id, header, content, date, upvotes, downvotes, tags FROM questions WHERE id LIKE $id;");						
+			$stmt -> bindParam('id', $id);
+		
+			$stmt -> execute();
+			
+			// while there is a question with specified id
+			while($result = $stmt -> fetch()){
+                $q = new Question();
+
+				$q->set_id($result[0]);
+                $q->set_accountId($result[1]);
+                $q->set_header($result[2]);
+                $q->set_content($result[3]);
+                $q->set_date($result[4]);
+                $q->set_upvotes($result[5]);
+                $q->set_downvotes($result[6]);
+                $q->set_tags($result[7]);
+
+
+
+                // get the username from the accounts table
+                
+                $questionArray[] = $q;
+			}
+		}
+		catch(PDOException $e){
+			$log->lwrite($e -> getMessage());
+		}
+		finally{
+			unset($pdo);
+		}
+		// returns the questions array
+		return $questionArray;
+	}
+
+	/**
 	* Updates a question in the question table of the Database
 	*
 	* @param $question		Question object
