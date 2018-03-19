6d8e8879c9c9ec69a11eef592a42325658245d20
Wissem Bahloul <wi55em@hotmail.com>
2018-03-18 23:36:43 -0400

    Fixed path issue in footer.php

--- public_html/home_page/footer.php
+++ public_html/home_page/footer.php
@@ -3,7 +3,7 @@
   <head>
     <title>Ask It! Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../css/foote.css">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
   </head>
 
   <body>
