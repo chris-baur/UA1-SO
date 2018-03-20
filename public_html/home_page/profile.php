0be470fdb39e6d4bd62c9577f8002c18359ef338
Wissem Bahloul <wi55em@hotmail.com>
2018-03-20 12:33:25 -0400

    Added change bio to profile settings
    Related to #28

--- public_html/home_page/profile.php
+++ public_html/home_page/profile.php
@@ -4,7 +4,7 @@
 	<html>
 		<head>
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" type="text/css" href="../css/profile.css">
+			<link rel="stylesheet" type="text/css" href="../css/profil.css">
 		</head>
 		<body>
 
@@ -49,31 +49,6 @@
 					?>
 						<p id = "changePic"> </p>
 
-						<script>
-							function changePicture() {
-								document.getElementById("ChangePass").innerHTML = "";
-								document.getElementById("changePic").innerHTML = '<form enctype="multipart/form-data" action="uploadImage.php" method="POST" style = "padding-bottom: 2%"><input name="userfile" type="file" style = "font-size: 18px; width: 50%"/><input type=submit name = "modify" value="Modify" onclick = "noshiet()" style = "font-size: 18px"/></form><input type=submit value="Cancel" onclick = "cancelChange()" style = "font-size: 18px"/>';
-							}
-
-							function pictureChanged() {
-								document.getElementById("changePic").outterHTML = "";
-							}
-
-							function changePassword() {
-								document.getElementById("changePic").innerHTML = "";
-								document.getElementById("ChangePass").innerHTML = '<form action="changePassword.php" method="POST">Current Password <input type = "text" id = "ChangePass" placeholder = "Password"><br> New Password <input type = "text" id = "changePass" placeholder = "Password"/><br><input type = "submit" id = "subChange" value = "Submit"/><input type = "submit" id = "subCancel" onclick = "cancelPass()" value = "Cancel"/></form>';
-							}
-
-							function cancelPass() {
-								document.getElementById("ChangePass").innerHTML = "";
-							}
-
-							function cancelChange() {
-								document.getElementById("changePic").innerHTML = "";
-							}
-
-						</script>
-
 					</div>
 
 					<div class = "column2">	 
@@ -82,14 +57,58 @@
 								<input type="submit" id = "password" onclick = "changePassword()" value = "Change Password"/>	
 							</li>
 							<li>
-								<input type=submit id = "button" onclick = "changePicture()" value="Change profile picture"/>
+								<input type=submit id = "profile" onclick = "changePicture()" value="Change profile picture"/>
 							</li>
-							
-
+							<li>
+								<input type=submit id = "bio" onclick = "changeBio()" value="Change your bio"/>
+							</li>
 					</div>
 
-				</div>
+
+
+				</div>					
+
+				<div id = "changeBio"> </div>
+				
 		</div>
+
+		<script>
+			
+			function changePicture() {
+				document.getElementById("ChangePass").innerHTML = "";
+				document.getElementById("changeBio").innerHTML = "";
+				document.getElementById("changePic").innerHTML = '<form enctype="multipart/form-data" action="uploadImage.php" method="POST" style = "padding-bottom: 2%"><input name="userfile" type="file" style = "font-size: 18px; width: 50%"/><input type=submit name = "modify" value="Modify" onclick = "noshiet()" style = "font-size: 18px"/></form><input type=submit value="Cancel" onclick = "cancelChange()" style = "font-size: 18px"/>';
+			}
+
+			function pictureChanged() {
+				document.getElementById("changePic").outterHTML = "";
+			}
+
+			function changePassword() {
+				document.getElementById("changeBio").innerHTML = "";
+				document.getElementById("changePic").innerHTML = "";
+				document.getElementById("ChangePass").innerHTML = '<form action="changePassword.php" method="POST">Current Password <input type = "text" id = "ChangePass" placeholder = "Password"><br> New Password <input type = "text" id = "changePass" placeholder = "Password"/><br><input type = "submit" id = "subChange" value = "Submit"/><input type = "submit" id = "subCancel" onclick = "cancelPass()" value = "Cancel"/></form>';
+			}
+
+			function changeBio() {
+				document.getElementById("ChangePass").innerHTML = "";
+				document.getElementById("changePic").innerHTML = "";
+				document.getElementById("changeBio").innerHTML = '<br>Describe Yourself<form action="uploadBio.php" method="POST" style = "padding-bottom: 2%"><textarea rows = 5 cols = 50 placeholder = "Enter your description here" maxlength = 500 style = "font-size: 18px; height:50%"></textarea><br><input type=submit value="Submit" style = "font-size: 18px"/><input type=submit value="Cancel" onclick = "cancelBio()" style = "font-size: 18px; margin-left: 2%"/></form>';
+			}
+
+			function cancelPass() {
+				document.getElementById("ChangePass").innerHTML = "";
+			}
+
+			function cancelChange() {
+				document.getElementById("changePic").innerHTML = "";
+			}
+
+			function cancelBio() {
+				document.getElementById("changeBio").innerHTML = "";
+			}
+
+		</script>
 
 		</body>
 	</html>
