8af3c4d5cb4bff1566888d93d9c3dabe4e49bb9d
Wissem Bahloul <wi55em@hotmail.com>
2018-03-28 23:13:10 -0400

    Added General Information (fname, lname, prof) to modify
    Related to issue #28

--- public_html/home_page/profile.php
+++ public_html/home_page/profile.php
@@ -4,7 +4,7 @@
 	<html>
 		<head>
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" type="text/css" href="../css/profile.css">
+			<link rel="stylesheet" type="text/css" href="../css/prof.css">
 		</head>
 		<body>
 
@@ -25,8 +25,7 @@
 			?>
 
 			<div class = "title"> Account Settings
-				<br>
-				<a id = "titleName"><?php echo $_SESSION['username']?>'s profile </a>
+				
 				<div class = "columns">
 
 					<div class = "column1">
@@ -52,12 +51,23 @@
 						<button type=button id = "profile" class = "btn btn-primary btn-block" onclick = "changePicture()"> Change profile picture </button>
 						<button type=button id = "profile" class = "btn btn-primary btn-block" onclick = "changePassword()"> Modify password </button>
 						<button type=button id = "profile" class = "btn btn-primary btn-block" onclick = "changeBio()"> Add or change your description </button>
+						<button type=button id = "profile" class = "btn btn-primary btn-block" onclick = "changeInfo()"> Modify my general informations </button>
 						
 					</div>
 
-					<div class = "column2">	 
-							
+					<div class = "column2">	
 						
+						<div id = "gen">Your General Informations</div>
+						<p id = text> 
+							<?php
+								$fname = $account->getName();
+								$lname = $account->getLastName();
+								$profession = $account->getProfession();
+								$gender = $account->getGender();
+								echo 'First Name ' ;
+							?>
+						</p>
+						<p id = info> </p>
 					</div>
 
 				</div>					
@@ -71,7 +81,6 @@
     						echo "<br><p style='color:green;'>".$_GET['errorMessage']."</p>";
     					} else {
     						echo "<br><p style='color:red;'>".$_GET['errorMessage']."</p>";
-
     					}
 					}
 				?>
@@ -84,7 +93,8 @@
 				function changePicture() {
 					document.getElementById("ChangePass").innerHTML = "";
 					document.getElementById("changeBio").innerHTML = "";
-					document.getElementById("changePic").innerHTML = '<form enctype="multipart/form-data" action="../home_page/profile/uploadImage.php" method="POST" style = "padding-bottom: 2%"><input name="userfile" type="file" style = "font-size: 18px; width: 50%"/><input type=submit name = "modify" value="Modify" onclick = "noshiet()" style = "font-size: 18px"/></form><input type=submit value="Cancel" onclick = "cancelChange()" style = "font-size: 18px"/>';
+					document.getElementById("info").innerHTML = "";
+					document.getElementById("changePic").innerHTML = '<form enctype="multipart/form-data" action="../home_page/profile/uploadImage.php" method="POST" style = "padding-bottom: 2%; padding-top: 2%"><input name="userfile" type="file" style = "font-size: 18px; width: 60%; margin-top:2%;"/><br><input type=submit name = "modify" class = "btn btn-gray btn-sm" id = "subChange" value="Submit" style = "font-size: 14px"/><input type=submit id = subCancel class = "btn btn-gray btn-sm" value="Cancel" onclick = "cancelChange()" style = "font-size: 14px; margin-top: 2%"/></form>';
 				}
 
 				function pictureChanged() {
@@ -94,13 +104,22 @@
 				function changePassword() {
 					document.getElementById("changeBio").innerHTML = "";
 					document.getElementById("changePic").innerHTML = "";
-					document.getElementById("ChangePass").innerHTML = '<br><form action="profile/changePassword.php" method="POST"> Current Password <input type = "password" class = "btn btn-gray btn-sm" name = "currentPass" placeholder = "Password"><br>New Password <input type = "password" class = "btn btn-gray btn-sm" name = "newpassword1" placeholder = "Password" style = "margin-top: 1%"/><br>Retype New Password <input type = "password" class = "btn btn-gray btn-sm" name = "newpassword2" placeholder = "Password" style = "margin-top: 1%"/><br><input type = "submit" class = "btn btn-gray btn-sm" id = "subChange" value = "Submit"/><input type = "submit" class = "btn btn-gray btn-sm" id = "subCancel" onclick = "cancelPass()" value = "Cancel" style = "margin-top: 2%"/></form>';
+					document.getElementById("info").innerHTML = "";
+					document.getElementById("ChangePass").innerHTML = '<br><form action="profile/changePassword.php" method="POST"> Current Password <br> <input type = "password" class = "btn btn-gray btn-sm" name = "currentPass" placeholder = "Current Password" style = "width: 100%"><br>New Password <br><input type = "password" class = "btn btn-gray btn-sm" name = "newpassword1" placeholder = "New Password" style = "margin-top: 1%; width: 100%"/><br>Retype New Password <br><input type = "password" class = "btn btn-gray btn-sm" name = "newpassword2" id = "focus" placeholder = "New Password" style = "margin-top: 1%; width:100%"/><br><input type = "submit" class = "btn btn-gray btn-sm" id = "subChange" value = "Submit"/><input type = "submit" class = "btn btn-gray btn-sm" id = "subCancel" onclick = "cancelPass()" value = "Cancel" style = "margin-top: 2%"/></form>';
 				}
 
 				function changeBio() {
 					document.getElementById("ChangePass").innerHTML = "";
 					document.getElementById("changePic").innerHTML = "";
-					document.getElementById("changeBio").innerHTML = '<br>Describe Yourself<form action="profile/uploadBio.php" method="POST" style = "padding-bottom: 2%"><textarea rows = 5 cols = 50 class = "btn btn-gray btn-sm" name = bio placeholder = "Enter your description here" maxlength = 300 style = "font-size: 18px; height:50%"></textarea><br><input type=submit class = "btn btn-gray btn-sm" value="Submit" style = "font-size: 18px"/><input type=submit class = "btn btn-gray btn-sm" value="Cancel" onclick = "cancelBio()" style = "font-size: 18px; margin-left: 2%"/></form>';
+					document.getElementById("info").innerHTML = "";
+					document.getElementById("changeBio").innerHTML = '<br>Describe Yourself<form action="profile/uploadBio.php" method="POST" style = "padding-bottom: 2%"><textarea rows = 5 cols = 50 class = "btn btn-gray btn-sm" name = bio placeholder = "Enter your description here" maxlength = 300 style = "font-size: 18px; height:50%; width: 80%"></textarea><br><input type=submit id = "subChange" class = "btn btn-gray btn-sm" value="Submit" style = "font-size: 18px"/><input type=submit id = "subCancel" class = "btn btn-gray btn-sm" value="Cancel" onclick = "cancelBio()" style = "font-size: 18px; margin-left: 2%; margin-top: 2%"/></form>';
+				}
+
+				function changeInfo() {
+					document.getElementById("ChangePass").innerHTML = "";
+					document.getElementById("changePic").innerHTML = "";
+					document.getElementById("changeBio").innerHTML = "";
+					document.getElementById("info").innerHTML = '<form action="#" method="POST" id = "info" style="margin-left: 2%"><label for="fname" style="margin-top: 2%">UserName</label><input type="text" id="uname" name="username" placeholder= "<?php echo $_SESSION['username'] ?>"><br><label for="fname" style="margin-top: 2%">First Name</label><input type="text" id="fname" name="firstname" placeholder="Your name.."><br><label for="lname" style="margin-top: 2%">Last Name</label><input type="text" id="lname" name="lastname" placeholder="Your last name.."><br><label for="lname" style="margin-top: 2%">Profession</label><input type="text" id="lname" name="lastname" placeholder="<?php echo $account->getProfession() ?>"><br><label for="lname" style="margin-top: 2%">Gender</label><input type="text" id="lname" name="lastname" placeholder="<?php echo $account->getGender() ?>"><br><br><input type="submit" value="Submit"><input type="submit" value="Cancel" onclick = "cancelInfo()" style = "background-color: red;"></form>';
 				}
 
 				function cancelPass() {
@@ -113,6 +132,10 @@
 
 				function cancelBio() {
 					document.getElementById("changeBio").innerHTML = "";
+				}
+
+				function cancelInfo() {
+					document.getElementById("info").innerHTML = "";
 				}
 
 			</script>
