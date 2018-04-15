b09ccbd68d0dcdd7a7b9f8bfcec651cfc5c7846a
chris-baur <chriskbaur@gmail.com>
2018-04-15 13:04:04 -0400

    Added QuestionThreadDatabaseTesting
    Added a test, missing a few still

---
+++ tests/questionThreadDatabaseTest.php
@@ -0,0 +1,65 @@
+<?php
+
+//include(dirname(__FILE__)."/../private/controllers/account_controller.php");
+
+class questionThreadDatabaseTest extends PHPUnit\Framework\TestCase{
+    public function testGetQuestionThread(){
+        $qtc = new QuestionThreadController();
+        $qt = $qtc::getQuestionThread(1);
+        $expectedName = 'john117';
+        $actualName = $qt->getQuestionName();
+        $this->assertEquals($expectedName, $actualName);
+        
+    }
+
+    // public function testAddQuestion(){
+    //     $qc = new QuestionThreadController();
+    //     $q = new Question();
+    //     $id = 1;
+    //     $expectedHeader = 'A random header for testing'; 
+    //     $q->setHeader($expectedHeader);
+    //     $id = $qc::addQuestion($q);
+    //     $addedQ = $qc::getQuestionById($id);
+    //     $actualHeader = $addedQ->getHeader();
+
+    //     $this->assertEquals($expectedHeader, $actualHeader);
+    // }
+
+    // public function testGetQuestionsByAccount(){
+    //     $qc = new QuestionController();
+    //     $account = new Account();
+    //     $account->setId(1);
+    //     $expectedNumber = 5;
+        
+    //     $actualNumber = sizeof($qc::getQuestionsByAccount($account));
+    //     $this->assertEquals($expectedNumber, $actualNumber);
+    // }
+
+    // public function testGetQuestionsByContent(){
+    //     $qc = new QuestionController();
+    //     $expectedNumber = 1;
+    //     $content = 'Foo FIghters';
+        
+    //     $actualNumber = sizeof($qc::getQuestionsByContent($content));
+    //     $this->assertEquals($expectedNumber, $actualNumber);        
+    // }
+    // public function testGetQuestionsByHeader(){
+    //     $qc = new QuestionController();
+    //     $expectedNumber = 1;
+    //     $header = 'Gooey';
+        
+    //     $actualNumber = sizeof($qc::getQuestionsByHeader($header));
+    //     $this->assertEquals($expectedNumber, $actualNumber);        
+    // }
+    // public function testUpdateQuestion(){
+    //     $qc = new QuestionController();
+    //     $q = $qc::getQuestionById(1);
+    //     $newHeader = 'klkfdnksdvk';
+    //     $q->setHeader($newHeader);
+    //     $qc::updateQuestion($q);
+    //     $q = $qc::getQuestionById(1);
+
+    //     $updatedHeader = $q->getHeader();
+    //     $this->assertEquals($newHeader, $updatedHeader);
+    // }
+}
