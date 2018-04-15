d7489ddf553b8674fc89566b2c1d53b474896bec
Wissem Bahloul <wi55em@hotmail.com>
2018-04-15 16:24:15 -0400

    Body is responsive to browser's size
    Related to issue #28

--- public_html/views/profile.php
+++ public_html/views/profile.php
@@ -4,7 +4,7 @@
 	<html>
 		<head>
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" type="text/css" href="../css/profile2.css">
+			<link rel="stylesheet" type="text/css" href="../css/profile3.css">
 		</head>
 		<body>
 			
