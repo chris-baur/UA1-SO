260bfe65c1476db3c90531a43ef726360b9b6467
Wissem Bahloul <wi55em@hotmail.com>
2018-04-14 22:37:48 -0400

    Fixed link related issues with header
    #Related to issue #10

--- public_html/home_page/menu_template.php
+++ public_html/home_page/menu_template.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>Ask It! About</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-    	<link rel="stylesheet" type="text/css" href="../css/header.css">
+    	<link rel="stylesheet" type="text/css" href="../css/header1.css">
     </head>
 
  	<body>
